fa2b7f5c4876e8569967aab620fc67d7b7156b13
Nicholas Grogg <ngrogg@pop-os.localdomain>
2024-05-18 18:57:22 -1000

    Initial commit on testing branch, added MySQL/MariaDB and WordPress prompt to generic-lamp.sh

--- generic-lamp.sh
+++ generic-lamp.sh
@@ -24,40 +24,71 @@ function helpFunction(){
     "* Rocky " \
     "* Ubuntu" \
     " "
-
 }
 
 ## Function to run program
 function runProgram(){
 	printf "%s\n" \
 	"Configure" \
-	"----------------------------------------------------"
+	"----------------------------------------------------" \
+    " "
+
+    ### Prompt user to choose a Database Technology
+	printf "%s\n" \
+	"MySQL or MariaDB?" \
+	"----------------------------------------------------" \
+    "Choose a Database technology:" \
+    "1: MySQL " \
+    "2: MariaDB " \
+    "Enter 1 or 2: "
+    read databaseTech
+    #### Check input
+
+    ### Prompt user to confirm if WordPress should be installed
+	printf "%s\n" \
+	"WordPress install?" \
+	"----------------------------------------------------" \
+    "Confirm if WordPress should be installed:" \
+    "1: Install WordPress" \
+    "0: Do NOT install WordPress" \
+    "Enter 1 or 0: "
+    read wordpressBool
+    #### Check input
 
     ### Check Distro, make distro specific changes
+	printf "%s\n" \
+	"Confirming passed Distro" \
+	"----------------------------------------------------" \
+    " "
+
     case "$1" in
             [Dd]ebian)
                     printf "%s\n" \
                     "Debian" \
                     "----------------------------------------------------" \
                     " "
+                    packageManager="apt"
                     ;;
             [Uu]buntu)
                     printf "%s\n" \
                     "Ubuntu" \
                     "----------------------------------------------------" \
                     " "
+                    packageManager="apt"
                     ;;
             [Ff]edora)
                     printf "%s\n" \
                     "Fedora" \
                     "----------------------------------------------------" \
                     " "
+                    packageManager="dnf"
                     ;;
             [Rr]ocky)
                     printf "%s\n" \
                     "Rocky" \
                     "----------------------------------------------------" \
                     " "
+                    packageManager="dnf"
                     ;;
             *)
                     printf "%s\n" \
